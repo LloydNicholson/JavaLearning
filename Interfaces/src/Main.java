678430b241d39add110cfef5006b9be06eb16f95
LloydNicholson <nichlloyd@gmail.com>
2019-01-07 11:02:50 +0200

    added code to interfaces

--- Interfaces/src/Main.java
+++ Interfaces/src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		ITelephone lloydsPhone;
+		lloydsPhone = new DeskPhone(123456);
+		lloydsPhone.powerOn();
+		lloydsPhone.callPhone(123456);
+		lloydsPhone.answer();
 		
-
+		lloydsPhone = new MobilePhone(202333);
+		lloydsPhone.powerOn();
+		lloydsPhone.callPhone(202333);
+		lloydsPhone.answer();
+		
 	}
+	
 
+	
 }
+	
+	
